2fa2cfcf5478c360c2e6514216e1e0a6cfd60f4f
Leon So <Leon_so7@hotmail.com>
2022-03-11 01:56:41 -0500

    Update Amazon 2022 internship description

--- app/src/components/aboutSection/Amazon2022Description.js
+++ app/src/components/aboutSection/Amazon2022Description.js
@@ -9,7 +9,17 @@ function Amazon2022Description() {
     on the Datapath team design, build, and maintain highly scalable distributed systems that execute business \
     logic, authored by 1400+ teams across Amazon. Datapath is a low latency, high throughput system that \
     processes over 20 million transactions per second across 200K+ hosts in real-time.";
-    const props = { companyName: companyName, role: role, briefDescription: briefDescription };
+    const roleDescription = [
+        "Worked on Datapath Efficiency, a team focused on improving the cost effectiveness of the Datapath \
+        platform by reducing infrastructure monthly rate (IMR) and optimizing costs per billion transactions",
+        "Designed, architected, and developed a system to identify ineffective caches by continuously \
+        aggregating, processing, and auditing time series metrics such as cache hit rate, serialization, \
+        and deserialization costs, read and miss latency, etc",
+        "Implemented a web-based portal acting as an observability hub to provide engineers and \
+        stakeholders with visibility on ineffective caches and cache behavior to allow for data-driven \
+        decisions on tuning and optimizing caching"
+    ]
+    const props = { companyName: companyName, role: role, briefDescription: briefDescription, roleDescription: roleDescription };
     return (
         <div className="experienceDiv">
             <ExperienceDescription {...props} />
