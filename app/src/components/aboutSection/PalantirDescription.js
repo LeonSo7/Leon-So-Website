b66a389853e5d3fe8d339422bc1e94cd869b3928
Leon So <Leon_so7@hotmail.com>
2022-04-05 21:09:46 -0700

    Update palantir internship description

--- app/src/components/aboutSection/PalantirDescription.js
+++ app/src/components/aboutSection/PalantirDescription.js
@@ -3,7 +3,7 @@ import '../../styles/components/aboutSection/ExperienceDescription.css'
 
 function PalantirDescription() {
     const companyName = "Palantir";
-    const role = "(Incoming) Software Engineer Intern";
+    const role = "Software Engineer Intern";
     const briefDescription = "Palantir builds software that empowers organizations around the world to effectively integrate \
     their data, decisions, and operations... Software Engineers are involved throughout the product lifecycle \u2014 from idea generation, \
     design, and prototyping to execution and shipping while also being paired with a mentor dedicated to their growth and success.";
@@ -11,7 +11,7 @@ function PalantirDescription() {
     return (
         <div className="experienceDiv">
             <ExperienceDescription {...props} />
-            <p className="briefDescription">Incoming to Palantir Gotham.</p>
+            <p className="briefDescription">Palantir Gotham - Data Authoring Team</p>
         </div>
     );
 }
