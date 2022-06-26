d25b64cb30ee0b104183711f9b2b8302a765ced1
Leon So <Leon_so7@hotmail.com>
2022-06-26 12:18:16 -0700

    Update Meta brief description

--- app/src/components/aboutSection/MetaDescription.js
+++ app/src/components/aboutSection/MetaDescription.js
@@ -3,11 +3,9 @@ import '../../styles/components/aboutSection/ExperienceDescription.css'
 
 function MetaDescription() {
     const companyName = "Meta (Formerly Facebook)";
-    const role = "(Incoming) Software Engineer Intern";
-    const briefDescription = "Meta builds technologies that help people connect, find communities, and grow businesses. \
-    Software Engineers at Meta build new features and improve existing products that more than a billion people around the world use. \
-    They also work on highly impactful technical challenges to help the world be more open and connected. Software Engineers at Meta \
-    have the opportunity to solve unique, large-scale, highly complex technical problems.";
+    const role = "Software Engineer Intern";
+    const briefDescription = "PrestoDB is an open source distributed SQL query engine for running interactive \
+    analytic queries against data sources of all sizes ranging from gigabytes to petabytes.";
     const props = { companyName: companyName, role: role, briefDescription: briefDescription };
     return (
         <div className="experienceDiv">
